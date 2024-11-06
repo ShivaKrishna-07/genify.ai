3a83d16ffffb66d3bdee2e8b0f7425f16e645e44
Shiva Krishna <shivakrishna6032@gmail.com>
2024-11-06 22:19:36 +0530

    update title design

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,13 +7,13 @@ const Header = () => {
   return (
     <div className="p-5 bg-white shadow-sm border-b-2 flex justify-between items-center">
       <div>
-      <Link href={'/'} className="flex md:hidden flex-col items-center justify-center">
-        <Image alt="logo" width={50} height={50} src="/logo.svg" />
-        <h3 className="font-bold text-2xl">Genify<span className="font-bold text-3xl text-primary">.</span>ai</h3>
+      <Link href={'/'} className="flex gap-1 md:hidden flex-col items-center justify-center">
+        <Image alt="logo" width={40} height={40} src="/logo.svg" />
+        <h3 className="font-bold text-sm">Genify<span className="font-bold text-primary">.</span>ai</h3>
       </Link>
       </div>
       <div className="flex items-center gap-5">
-        <Link href={'/dashboard/billing'} className="bg-primary  p-2 rounded-full text-sm text-white px-2">
+        <Link href={'/dashboard/billing'} className="bg-primary  p-2 rounded-full text-xs text-white px-2">
           👑Join Membership just for $9.99
         </Link>
         <UserButton />
